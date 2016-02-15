1ee1e2247944290a4f39e3444056c78e841d406c
Adrien Cadet <acadet@live.fr>
2016-02-15 10:47:35 -0800

    Sets up first iteration

--- main.go
+++ main.go
@@ -10,13 +10,20 @@ import (
 )
 
 var appConfiguration = map[string] string {
-    "blog_root": "http://www.coshx.com",
-    "blog_homepage": "http://www.coshx.com/blog/",
+    //"website_root": "http://www.coshx.com",
+    "website_root": "http://localhost:4567/",
+    //"blog_homepage": "http://www.coshx.com/blog/",
+    "blog_homepage": "http://localhost:4567/blog/",
+    "min_word_length": "3",
 }
+
+// struct Type Article {
+
+// }
 
 var articles = list.New()
 
-func enginePage(href string, whenDone chan bool) {
+func enginePost(href string, whenDone chan bool) {
     doc, err := goquery.NewDocument(href)
 
     if err != nil {
@@ -25,7 +32,10 @@ func enginePage(href string, whenDone chan bool) {
         return
     }
 
-    text := doc.Find(".post-content").Text()
+    //author := doc.Find(".james-author").Text()
+    //headline := doc.Find(".james-headline").Text()
+    text := doc.Find(".james-content").Text()
+    //timestamp, _ := doc.Find(".james-date").Attr("data-timestamp")
 
     buffer := ""
     var hash = make(map[string] int)
@@ -34,7 +44,7 @@ func enginePage(href string, whenDone chan bool) {
         if isMatching, _ := regexp.MatchString("[a-zA-Z0-9]", c); isMatching == true {
             buffer += c
         } else if buffer != "" {
-            if xstrings.Len(buffer) >= 4 {
+            if xstrings.Len(buffer) >= /*appConfiguration["min_word_length"]*/ 3 {
                 buffer = strings.ToLower(buffer)
                 e, exists := hash[buffer]
                 if exists {
@@ -51,42 +61,53 @@ func enginePage(href string, whenDone chan bool) {
     whenDone <- true
 }
 
-func getHome(whenDone chan bool) {
-    doc, _ := goquery.NewDocument(appConfiguration["blog_homepage"])
+func fetchHome(whenDone chan bool) {
+    doc, err := goquery.NewDocument(appConfiguration["blog_homepage"])
+
+    if err != nil {
+        fmt.Printf("Impossible to reach homepage")
+        whenDone <- false
+        return
+    }
 
     var buffer = make(chan bool)
     started := 0
     ended := 0
+
     go func() {
-        select {
-        case <- buffer:
-            ended++
-            if ended == started {
-                close(buffer)
-                if articles.Len() > 0 {
-                    element := articles.Front()
-                    for {
-                        fmt.Printf("%v\n", element)
-                        element = element.Next()
-                        if element == nil {
-                            break
+        for {
+            select {
+            case <- buffer:
+                ended++
+                if ended == started {
+                    close(buffer)
+                    if articles.Len() > 0 {
+                        element := articles.Front()
+                        for {
+                            fmt.Printf("%v\n", element)
+                            element = element.Next()
+                            if element == nil {
+                                break
+                            }
                         }
                     }
+                    whenDone <- true
                 }
-                whenDone <- true
             }
         }
     }()
 
-    doc.Find(".blogposts a").Each(func (i int, s *goquery.Selection) {
-        link, _ := s.Attr("href")
-        started++
-        go enginePage(appConfiguration["blog_root"] + link, buffer)
+    doc.Find(".james-blogposts .james-post").Each(func (i int, s *goquery.Selection) {
+        link, isExisting := s.Attr("href")
+        if isExisting {
+            started++
+            go enginePost(appConfiguration["website_root"] + link, buffer)
+        }
     })
 }
 
 func main() {
     var c = make(chan bool)
-    go getHome(c)
+    go fetchHome(c)
     <- c
 }
